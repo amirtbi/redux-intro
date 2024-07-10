ce042ec73d89112e14e273a7060a5986a3b3a7e1
Amirhosein Torabi <torabi46@gmail.com>
2024-07-10 12:04:57 +0330

    Setup redux toolkit

--- src/store-legacy.tsx
+++ src/store-legacy.tsx
@@ -1,17 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware, combineReducers,createStore } from "redux";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from '@redux-devtools/extension';
 import { useDispatch} from "react-redux";
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customer/customerSlice";
 
 
+const reducers = combineReducers({account:accountReducer,customer:customerReducer});
+const store = createStore(reducers,composeWithDevTools(applyMiddleware(thunk)))
 
-const store = configureStore({
-    reducer:{
-        account:accountReducer,
-        customer:customerReducer
-    }
-})
+
 export default store
+
+
+
 /// types 
 
 export type RootState = ReturnType<typeof store.getState>;
